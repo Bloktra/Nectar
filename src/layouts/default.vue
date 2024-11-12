b7f24fff0a71e3c7b9d9a61d68624aa81fd61fc2
Liam Robinson <liamrobinson5@web.de>
2024-11-12 19:24:11 +0100

    first commit

---
+++ src/layouts/default.vue
@@ -0,0 +1,12 @@
+<template>
+  <div
+    class="min-h-screen bg-gradient-to-b from-neutral-950 to-zinc-800 text-white"
+  >
+    <Navigation />
+    <Hero />
+    <FeaturedBuilds />
+    <Categories />
+    <TrendingSection />
+    <Footer />
+  </div>
+</template>
